15937b614aad65efb13e3fde21de144cdf65b28f
Kzquandary <kzquandary@gmail.com>
2024-05-22 22:30:52 +0700

    add user auth

--- helper/error_handler.go
+++ helper/error_handler.go
@@ -1,14 +1,46 @@
 package helper
 
 import (
-	"net/http"
 	"backendgreeve/constant"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
 func ConvertResponseCode(err error) int {
 	switch err {
+
+	// Users Error
+	case constant.UserNotFound:
+		return http.StatusNotFound
+	case constant.ErrEmptyLogin:
+		return http.StatusBadRequest
+	case constant.ErrLoginIncorrectPassword:
+		return http.StatusUnauthorized
+	case constant.ErrEmptyEmailRegister:
+		return http.StatusBadRequest
+	case constant.ErrEmptyPasswordRegister:
+		return http.StatusBadRequest
+	case constant.ErrEmptyAddressRegister:
+		return http.StatusBadRequest
+	case constant.ErrEmptyNameRegister:
+		return http.StatusBadRequest
+	case constant.ErrEmptyGenderRegister:
+		return http.StatusBadRequest
+	case constant.ErrEmailAlreadyExist:
+		return http.StatusConflict
+	case constant.ErrUsernameAlreadyExist:
+		return http.StatusConflict
+	case constant.ErrRegister:
+		return http.StatusInternalServerError
+	case constant.ErrGenerateJWT:
+		return http.StatusInternalServerError
+	case constant.ErrValidateJWT:
+		return http.StatusUnauthorized
+	case constant.ErrHashPassword:
+		return http.StatusInternalServerError
+	case constant.ErrEmptyPhoneRegister:
+		return http.StatusBadRequest
 
 	// Default
 	default:
--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"backendgreeve/config"
+	"backendgreeve/constant/route"
 	"backendgreeve/features/users"
 	MidtransWebhook "backendgreeve/features/webhook"
 
@@ -11,14 +12,14 @@ import (
 
 func RouteUser(e *echo.Echo, uh users.UserHandlerInterface, cfg config.GreeveConfig) {
 
-	e.POST("/register", uh.Register())
-	e.POST("/login", uh.Login())
-	e.POST("/forgot-password", uh.ForgotPassword())
-	e.POST("/verify-otp", uh.VerifyOTP())
-	e.POST("/reset-password", uh.ResetPassword())
+	e.POST(route.UserRegister, uh.Register())
+	e.POST(route.UserLogin, uh.Login())
+	e.POST(route.UserForgotPassword, uh.ForgotPassword())
+	e.POST(route.UserVerifyOTP, uh.VerifyOTP())
+	e.POST(route.UserResetPassword, uh.ResetPassword())
 
-	e.PUT("/update-user", uh.Update(), echojwt.JWT([]byte(cfg.JWT_Secret)))
-	e.DELETE("/delete-user", uh.Delete(), echojwt.JWT([]byte(cfg.JWT_Secret)))
+	e.PUT(route.UserPath, uh.Update(), echojwt.JWT([]byte(cfg.JWT_Secret)))
+	e.DELETE(route.UserPath, uh.Delete(), echojwt.JWT([]byte(cfg.JWT_Secret)))
 }
 
 func PaymentNotification(e *echo.Echo, wh MidtransWebhook.MidtransNotificationHandler, cfg config.GreeveConfig) {
