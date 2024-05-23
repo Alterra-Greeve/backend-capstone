192071a6c352cc710c40dca70d82e933c31fe2ff
ChandraWahyuR <chandrawahyurafialdi@gmail.com>
2024-05-23 14:35:39 +0700

    add admin auth v1

--- utils/database/migration.go
+++ utils/database/migration.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	DataAdmin "backendgreeve/features/admin/data"
 	DataUsers "backendgreeve/features/users/data"
 	WebhookData "backendgreeve/features/webhook/data"
 
@@ -11,5 +12,6 @@ func Migrate(db *gorm.DB) error {
 	db.AutoMigrate(&DataUsers.User{})
 	db.AutoMigrate(&DataUsers.VerifyOTP{})
 	db.AutoMigrate(&WebhookData.PaymentNotification{})
+	db.AutoMigrate(&DataAdmin.Admin{})
 	return nil
 }
